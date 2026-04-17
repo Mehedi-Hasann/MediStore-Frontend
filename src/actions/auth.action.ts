4bea9cf80606eaee1544850a16078bb8c7d608d2
Mehedi Hasan <u1904067@student.cuet.ac.bd>
2026-04-17 21:05:48 +0600

    add

--- src/actions/auth.action.ts
+++
@@ -1,8 +0,0 @@
-"use server"
-
-import { authService } from "@/services/auth.service"
-
-export const loginUser = async(value : {email : string, password : string}) => {
-  const res = await authService.loginUser(value);
-  return res;
-}
